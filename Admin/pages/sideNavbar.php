2d3f6bf741cd141ed3a259cc0c00d04139043150
Fahmi_Alamsyah_N <alamsyahfahmi99@gmail.com>
2023-01-06 08:29:56 +0700

    commit 6

--- Admin/pages/sideNavbar.php
+++ Admin/pages/sideNavbar.php
@@ -1,7 +1,7 @@
 <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
           <div class="sb-sidenav-menu">
             <div class="nav">
-              <div class="sb-sidenav-menu-heading">Table</div>
+              <div class="sb-sidenav-menu-heading">Data</div>
               <a class="nav-link" href="index.php">
                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                 Employee
@@ -10,10 +10,14 @@
                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                 Customer
               </a>
+              <br>
+
+              <div class="sb-sidenav-menu-heading">Our Menu</div>
               <a class="nav-link" href="menu.php">
                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                 Menu
               </a>
+              <br>
               <div class="sb-sidenav-footer">
             <div class="small">Group 9</div>
             FS_Resto
