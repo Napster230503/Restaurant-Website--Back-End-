74b2e0bfa21f9fb1e01e6d174ebccc35468cb57a
Saiful <kriegswar27@gmail.com>
2023-01-13 13:16:00 +0700

    kelar

--- Admin/pages/delete.php
+++ Admin/pages/delete.php
@@ -51,7 +51,7 @@ while($data = mysqli_fetch_assoc($result)) {
                             <?php endif; ?>
                         </div>
                     <div class="card-body">
-                    <form id="frmemp" method="post" action="proccess_delete.php">
+                    <form id="frmemp" method="post" action="process_delete.php">
                 <fieldset>
                             <div class="mb-3 row">
                                 <label for="code" class="col-sm-2 col-form-label">Employee Code</label>
