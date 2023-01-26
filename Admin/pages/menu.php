f27ec7c7f45414d162dd6c38d466579aba9e7fed
Saiful <kriegswar27@gmail.com>
2023-01-26 11:07:15 +0700

    dh

--- Admin/pages/menu.php
+++ Admin/pages/menu.php
@@ -102,13 +102,21 @@ if(!isset($_SESSION['masuk'])){
        ?>
       </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
     <script src="../../resource/js/scripts.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
     <script src="../../resource/assets/demo/chart-area-demo.js"></script>
     <script src="../../resource/assets/demo/chart-bar-demo.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
-    <script src="../../resource/js/datatables-simple-demo.js"></script>
+    <script src="../../resource/js/datatables-simple-demo.js"></script> -->
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script>
+    <script>
+        $(document).ready(function () {
+          $('#example').DataTable();
+      });
+    </script>
   </body>
 </html>
 <?php
